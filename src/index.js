0a71473b8c6236a07a1ad9f78346cc31c0199e78
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-26 01:53:47 +0630

    create store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
