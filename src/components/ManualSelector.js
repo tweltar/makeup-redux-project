b0c3dc22c4662237f8232e1aad1d4341f15ab8d2
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-28 03:01:17 +0630

    add ManualSelector

---
+++ src/components/ManualSelector.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ManualSelector.css';
+
+const ManualSelector = () => {
+    return (
+        <div className="ManualSelector">
+            <a href="" className="ByButton">By bla bla&nbsp;&nbsp;<i className="fas fa-angle-right RightArrow"></i></a>
+            <div className="SelectorContent">
+                <a>By Product</a>
+                <a>By Brand</a>
+            </div>
+        </div>
+);
+};
+
+export default ManualSelector;
